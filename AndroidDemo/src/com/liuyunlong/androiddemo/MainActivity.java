34ab7a846dac341f90184b8b49733a712fab5552
CloudDragon-liu <liuyunlong1126@163.com>
2015-09-08 23:03:36 +0800

    test

--- AndroidDemo/src/com/liuyunlong/androiddemo/MainActivity.java
+++ AndroidDemo/src/com/liuyunlong/androiddemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.liuyunlong.androiddemo;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
+
+	@SuppressLint("ShowToast")
+	public void doClick(View v) {
+		switch (v.getId()) {
+		case R.id.flagment_btn:
+			Toast.makeText(this, "哈哈", Toast.LENGTH_SHORT).show();
+			break;
+
+		default:
+			break;
+		}
 	}
 }
