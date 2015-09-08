b6a79c1ea2128c80934046e1ccd614ad9e3f60ce
CloudDragon-liu <liuyunlong1126@163.com>
2015-09-08 22:24:47 +0800

    修改包名

---
+++ Test/src/com/liuyunlong/Test.java
@@ -0,0 +1,11 @@
+package com.liuyunlong;
+
+/**
+ * @author : liuyunlong
+ * @version ��2015-9-8 ����9:05:19
+ * */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
