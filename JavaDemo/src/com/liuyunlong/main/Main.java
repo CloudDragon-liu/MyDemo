01d07e8060dbfff7e2e1e6379f1266ce1ad300a7
CloudDragon-liu <liuyunlong1126@163.com>
2015-09-09 09:54:18 +0800

    add main

---
+++ JavaDemo/src/com/liuyunlong/main/Main.java
@@ -0,0 +1,11 @@
+package com.liuyunlong.main;
+
+/** 
+* @author  : liuyunlong
+* @version ：2015-9-9 上午9:53:40 
+* */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
